57553239c8c4df0147bfad58669d3b5ae14d6738
Elias0Answorth <worldlolxd121@gmail.com>
2025-03-14 15:43:06 +0300

    feat: update fuction createRandomId to createRandomKey

--- src/components/list-component.tsx
+++ src/components/list-component.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
 interface User {
-    id: string;
+    id: number;
     name: string;
 }
 
-function createRandomid(): string{
+function createRandomKey(): string{
     return `id${+(new Date(3600 * 24)) + Math.random()}`
 }
 
 
 export function ListComponent() {
-    const [users, setUsers] = useState<User[]>([{ id: createRandomid(), name: "Tom"}, { id: createRandomid(), name: "Jon" }, { id: createRandomid(), name: "Sem"}])
+    const [users, setUsers] = useState<User[]>([{ id: 1, name: "Tom"}, { id: 2, name: "Jon" }, { id: 3, name: "Sem"}])
     return (
         <>
             <h1>Пользователи</h1>
             {users.map(user => (
-                <h2 key={user.id}>Уникальный идентификатор: {user.id} Имя: {user.name}</h2>
+                <h2 key={createRandomKey()}>Уникальный идентификатор: {user.id} Имя: {user.name}</h2>
             ))}
         </>
     )
